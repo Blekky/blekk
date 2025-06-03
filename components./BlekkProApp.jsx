0374e71b3e843b40d4b92ab8cb2c5831b04db697
Blekky <blekkpro@gmail.com>
2025-06-03 17:45:20 +0100

    Update BlekkProApp.jsx

--- components./BlekkProApp.jsx
+++ components./BlekkProApp.jsx
@@ -1,1 +1,9 @@
+export default function BlekkProApp() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '4rem' }}>
+      <h1>Welcome to BlekkPro</h1>
+      <p>A professional tool for PMU artists.</p>
+    </div>
+  );
+}
 
