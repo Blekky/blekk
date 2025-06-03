417627adc1e05dd90adfeb7899c0b20b9df5696e
Blekky <blekkpro@gmail.com>
2025-06-03 16:26:25 +0100

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main style={{ fontFamily: "sans-serif", textAlign: "center", padding: "2rem" }}>
+      <h1>Welcome to BlekkPro 👋</h1>
+      <p>Your PMU design app is almost ready!</p>
+      <p>🚧 This is your new homepage. You can customize it later.</p>
+    </main>
+  );
+}
